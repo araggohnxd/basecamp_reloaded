3402758915de54115586ec7a9b7f5edec7115357
maolivei <maolivei@student.42sp.org.br>
2022-03-23 17:38:56 +0100

    Exercise 13

---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/23 17:27:58 by maolivei          #+#    #+#             */
+/*   Updated: 2022/03/23 17:38:41 by maolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb == 0)
+		return (1);
+	if (nb < 0)
+		return (0);
+	return (nb * ft_recursive_factorial(nb - 1));
+}
