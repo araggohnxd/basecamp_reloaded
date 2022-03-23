a0687d1c6116a1943e96dc34f67020812adfff5f
maolivei <maolivei@student.42sp.org.br>
2022-03-23 17:27:28 +0100

    Exercise 12

---
+++ ex12/ft_iterative_factorial.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/23 17:23:48 by maolivei          #+#    #+#             */
+/*   Updated: 2022/03/23 17:27:14 by maolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+
+	if (nb < 0)
+		return (0);
+	result = 1;
+	while (nb > 0)
+		result *= nb--;
+	return (result);
+}
