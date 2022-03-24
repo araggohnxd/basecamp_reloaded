1264d256c6a2523bbd9a628f2d022d4c2f4f776a
araggohnxd <matheusnagato300@gmail.com>
2022-03-24 11:33:10 -0300

    Unecessary aesthetics ex22 xd

--- ex22/ft_abs.h
+++ ex22/ft_abs.h
@@ -6,11 +6,13 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 10:48:03 by maolivei          #+#    #+#             */
-/*   Updated: 2022/03/24 10:51:33 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/03/24 11:29:15 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_ABS_H
 # define FT_ABS_H
+
 # define ABS(Value) (Value > 0 ? Value : -Value)
+
 #endif
