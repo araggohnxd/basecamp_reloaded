a06285953bf78a45edd551526351f0bea64038bd
araggohnxd <matheusnagato300@gmail.com>
2022-03-25 01:02:46 -0300

    Exercise 27

---
+++ ex27/ft_display_file.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_display_file.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/25 00:37:52 by maolivei          #+#    #+#             */
+/*   Updated: 2022/03/25 01:01:58 by maolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <fcntl.h>
+
+void	ft_display_file(int fd)
+{
+	char	buffer;
+
+	if (fd == -1)
+		write(2, "Cannot read file.\n", 18);
+	while (read(fd, &buffer, 1))
+		write(1, &buffer, 1);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc > 2)
+		write(2, "Too many arguments.\n", 20);
+	else if (argc < 2)
+		write(2, "File name missing.\n", 19);
+	else
+		ft_display_file(open(argv[1], O_RDONLY));
+	return (0);
+}
